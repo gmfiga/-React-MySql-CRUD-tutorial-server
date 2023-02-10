9317060b9d1aa213067ac7329114d08da995781c
gmfiga <gmfiga@gmail.com>
2023-02-10 12:01:21 -0500

    changed server

--- index.js
+++ index.js
@@ -87,6 +87,6 @@ app.put("/books/:id", (req, res) => {
 });
 
 
-app.listen("https://bespoke-dusk-25f037.netlify.app/", () => {
+app.listen(8800, () => {
     console.log("Server is running on port 8800");
 });
